ed9efb6b406ee6beaee6a5c960801c43f29e128c
sunqiang85 <sunqiang85@163.com>
2018-08-23 21:44:49 +0800

    add parseArgs module

---
+++ parseArgs.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument("pos1", help="positional argument 1")
+parser.add_argument("-o", "--optional-arg", help="optional argument", dest="opt", default="default")
+args = parser.parse_args()
+print("positional arg:", args.pos1)
+print("optional arg:", args.opt)
