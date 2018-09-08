ddcb6dfcd394f1cbe6e05e2ac770d696e30f86ea
sunqiang85 <sunqiang85@163.com>
2018-09-08 09:45:27 +0800

    convert tab 4 space

--- parseArgs.py
+++ parseArgs.py
@@ -3,7 +3,12 @@ from __future__ import print_function
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("pos1", help="positional argument 1")
-parser.add_argument("-o", "--optional-arg", help="optional argument", dest="opt", default="default")
+parser.add_argument("-o", "--optional-arg", help="optional argument", dest="opt", default=None)
+parser.add_argument("-i", "--int-arg", help="int argument", dest="argint", default=0)
 args = parser.parse_args()
 print("positional arg:", args.pos1)
-print("optional arg:", args.opt)
+if args.opt:
+	print("optional arg:", args.opt)
+if args.argint:
+	print("int arg:", args.argint)
+	print("int arg type:", type(args.argint))
